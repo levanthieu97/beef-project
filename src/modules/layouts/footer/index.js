e623ba87735d85bce76a2ab06b7ae253278e0a00
Van Thieu <levanthieu97@gmail.com>
2021-05-26 01:41:05 +0700

    create footer component

--- src/modules/layouts/footer/index.js
+++ src/modules/layouts/footer/index.js
@@ -1,58 +1,98 @@
-import React from 'react';
-import Logo from '../../../assets/icons/logo';
-import {Link} from 'react-router-dom';
+import React from "react";
+import Logo from "../../../assets/icons/logo";
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import {
+  FaPhoneVolume,
+  FaStore,
+  FaFacebookF,
+  FaGooglePlusG,
+  FaInstagram,
+} from "react-icons/fa";
 
 const FooterComponent = () => {
-    return (
-        <footer className="site-footer">
-            <div className="component-container">
-                <div className="site-footer__top">
-                    <div className="site-footer__description">
-                        <h1 className="site-logo">NOW DELI</h1>
-                        {/* <p>House My Brand designs clothing for the young, the old & everyone in between – but most 
-                        importantly, for the fashionable</p> */}
-                        <div className="site-footer__follow">Follow us on</div>
-                        <ul className="site-footer__social-networks">
-                            <li>
-                                <Link to="#"><i className="icon-facebook"></i><span>acebook</span></Link>
-                            </li>
-                            <li><Link to="#"><i className="icon-instagram"></i><span>Instagram</span></Link></li>
-                        </ul>
+  return (
+    <footer className="site-footer">
+      <div className="component-container">
+        <div className="site-footer__top">
+          <div className="footer-top-wrapper row">
+            <div className="col-lg-4 col-md-6 col-sm-12 footer-column">
+              <div className="footer-widget logo-widget">
+                <figure className="footer-logo">
+                  <Link to="/">
+                    <h1 className="site-logo">NOW DELI</h1>
+                    {/* <img src="/beef/images/logo.png" alt="Footer Logo"></img> */}
+                  </Link>
+                </figure>
+                <div className="text">
+                  <p>
+                    We provide well shaped fresh, safe and organic meat from our
+                    farm in a very hygienic way for your pleasure. And our
+                    motivation is if our customers are happy then we are happy.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12 footer-column">
+              <div className="footer-widget contact-widget">
+                <ul className="info">
+                  <li>
+                    <IconContext.Provider value={{ className: "contact-icon" }}>
+                      <FaPhoneVolume></FaPhoneVolume>
+                    </IconContext.Provider>
+                    <p>Call Us</p>
+                    <h5>123-456-789</h5>
+                  </li>
+                  <li>
+                    <IconContext.Provider value={{ className: "contact-icon" }}>
+                      <FaStore></FaStore>
+                    </IconContext.Provider>
+                    <p>Address</p>
+                    <h5>1234 Hart Street, US</h5>
+                  </li>
+                </ul>
+                <ul className="social-links">
+                    <div className="title">
+                        <span>Social Media</span>
                     </div>
-            
-                    <div className="site-footer__links">
-                        {/* <ul>
-                            <li>Shopping online</li>
-                            <li><Link to="#">Order Status</Link></li>
-                            <li><Link to="#">Shipping and Delivery</Link></li>
-                            <li><Link to="#">Returns</Link></li>
-                            <li><Link to="#">Payment options</Link></li>
-                            <li><Link to="#">Contact Us</Link></li>
-                        </ul> */}
-                        <ul>
-                            <li>Information</li>
-                            <li><Link to="#">Gift Cards</Link></li>
-                            <li><Link to="#">Find a store</Link></li>
-                            <li><Link to="#">Newsletter</Link></li>
-                            <li><Link to="#">Bacome a member</Link></li>
-                            <li><Link to="#">Site feedback</Link></li>
-                        </ul>
-                        <ul>
-                            <li>Contact</li>
-                            <li><Link to="#">store@uikit.com</Link></li>
-                            <li><Link to="#">Hotline: +1 131 138 138</Link></li>
-                        </ul>
-                    </div>
+                  <li>
+                    <Link className="social-fb">
+                      <FaFacebookF></FaFacebookF>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="social-google">
+                      <FaGooglePlusG></FaGooglePlusG>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="social-instagram">
+                      <FaInstagram></FaInstagram>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12 footer-column">
+                <div className="footer-widget subscribe__content">
+                    <h4>Subscribe to our newsletter and receive exclusive offers every week</h4>
+                    <form className="subscribe__form">
+                        <input type="email" placeholder="Email" />
+                        <button type="submit" className="btn btn--rounded">Subscribe</button>
+                    </form>
                 </div>
             </div>
-            
-            <div className="site-footer__bottom">
-                <div className="component-container">
-                <p>NOW DELI - Copyright © {new Date().getFullYear()}.</p>
-                </div>
-            </div>
-        </footer>
-    )
-}
+          </div>
+        </div>
+      </div>
+
+      <div className="site-footer__bottom">
+        <div className="component-container">
+          <p>NOW DELI - Copyright © {new Date().getFullYear()}.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default React.memo(FooterComponent);
